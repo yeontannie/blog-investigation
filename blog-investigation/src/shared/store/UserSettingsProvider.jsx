bbab264fb823cef900e0fc0802a32f2e48a6a24e
Tetiana Stasiuk <tanastasuk35@gmail.com>
2022-08-11 15:58:17 +0300

    Update UserSettingsProvider.jsx

--- blog-investigation/src/shared/store/UserSettingsProvider.jsx
+++ blog-investigation/src/shared/store/UserSettingsProvider.jsx
@@ -7,12 +7,14 @@ const UserSettingsProvider = ({children}) => {
 
     const userSettingsContextValue = useMemo(() => {
         return {
-            ...state,
+            theme: state.theme,
+            language: state.language,
+            isLoggedIn: state.isLoggedIn,
             toggleTheme: (isChecked) => dispatch({type:'TOGGLE_THEME', payload:{isChecked}}),
             toggleLanguage: (language) => dispatch({type:'TOGGLE_LANGUAGE', payload:{language}}),
             setIsLoggedIn: (isLoggedIn) => dispatch({type:"SET_IS_LOGGED_IN", payload:{isLoggedIn}})
         }
-    }, [state, dispatch])
+    }, [state.theme, state.language, state.isLoggedIn, dispatch])
 
     return <UserSettingsContext.Provider value={userSettingsContextValue}>{children}</UserSettingsContext.Provider>
 }

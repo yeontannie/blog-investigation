ddafb3b31f88c99b8704d0aadf90c8d3fb35756e
Tetiana Stasiuk <tanastasuk35@gmail.com>
2022-08-15 19:21:58 +0300

    adding secrents to gitignor for a bilions time :anger:

--- blog-investigation/src/secret.env.js
+++ blog-investigation/src/secret.env.js
@@ -1,2 +1,6 @@
-export const CLIENT_ID = process.env.REACT_APP_OAUTH_CLIENT_ID || "449942686081-nqhlpdffi1rs4b78ohqi8cnhi8ug10rc.apps.googleusercontent.com"
-export const API_KEY = process.env.REACT_APP_BLOGGER_API_KEY || "AIzaSyA7H3vM3hQ0x7FElFDjGB68URJI5h9Q2AY"
+export const CLIENT_ID =
+  process.env.REACT_APP_OAUTH_CLIENT_ID ||
+  "449942686081-nqhlpdffi1rs4b78ohqi8cnhi8ug10rc.apps.googleusercontent.com";
+export const API_KEY =
+  process.env.REACT_APP_BLOGGER_API_KEY ||
+  "AIzaSyA7H3vM3hQ0x7FElFDjGB68URJI5h9Q2AY";

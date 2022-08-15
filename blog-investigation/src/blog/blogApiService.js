cbcc64bfe2e82d750dc60aec82ebbb9f97ac8eb9
Tetiana Stasiuk <tanastasuk35@gmail.com>
2022-08-15 20:43:40 +0300

    gitignor updated

--- blog-investigation/src/blog/blogApiService.js
+++ blog-investigation/src/blog/blogApiService.js
@@ -1,13 +1,16 @@
 import axios from "axios";
-import { API_KEY } from "../secret.env";
+import { API_KEY } from "../keys.env";
 
 export default class blogApiService {
-    static getBlogs(token) {
-        return axios.get(`https://www.googleapis.com/blogger/v3/users/self/blogs?key=${API_KEY}`, {
-            headers: {
-              'Authorization': `Bearer ${token}`,
-              'Content-type': 'application/json'
-            }
-        })
-    }
+  static getBlogs(token) {
+    return axios.get(
+      `https://www.googleapis.com/blogger/v3/users/self/blogs?key=${API_KEY}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-type": "application/json",
+        },
+      }
+    );
+  }
 }
